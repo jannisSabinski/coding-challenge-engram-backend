b584479b77079fb8f1370870defd2a7d27117b5d
Jannis Sabinski <jp.sabinski@gmail.com>
2026-04-02 13:50:36 +0200

    setup user entity + routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, HttpCode, HttpStatus, Patch, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { changePasswordDto, createUserDto } from './dto';
+import { GetUser } from 'src/decorator/get-user.decorator';
+import { User } from './user.entity';
+import { BasicAuthGuard } from 'src/auth/basic-auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('signUp')
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() dto: createUserDto): Promise<void>{
+    return await this.userService.create(dto);
+  }
+
+  @Delete()
+  @UseGuards(BasicAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  async deleteUser(@GetUser()user: User): Promise<void>{
+    return await this.userService.delete(user.id);
+  }
+
+  @Patch('password')
+  @UseGuards(BasicAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  async changePassword(
+    @GetUser() user: User,
+    @Body() dto: changePasswordDto,
+  ): Promise<void> {
+    return await this.userService.changePassword(user.id, dto.newPassword);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { createUserDto } from './dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UserService {
@@ -9,6 +11,24 @@ export class UserService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
+
+  async create(dto: createUserDto): Promise<void> {
+    if(await this.findByUsername(dto.name))throw new ConflictException('Username already taken');
+
+    const passwordHash = await argon2.hash(dto.password);
+    const newUser = this.userRepository.create({ name: dto.name, passwordHash});
+    await this.userRepository.save(newUser);
+  }
+
+  async delete(userId: string): Promise<void> {
+    await this.userRepository.delete(userId);
+    //TODO rewrite when bucket + files exist
+  }
+
+  async changePassword(userId: string, newPassword: string): Promise<void> {
+    const passwordHash = await argon2.hash(newPassword);
+    await this.userRepository.update(userId, {passwordHash: passwordHash});
+  }
 
   async findByUsername(name: string): Promise<User | null> {
     return this.userRepository.findOne({ where: { name } });
