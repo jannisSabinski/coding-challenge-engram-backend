afe0e8abcdb8ddd119822861d16cea6f3d7a1ac3
Jannis Sabinski <jp.sabinski@gmail.com>
2026-04-02 11:59:10 +0200

    fixed vulnerable dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PictureModule } from './picture/picture.module';
 import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 
 @Module({
   imports: [AuthModule, UserModule, PictureModule, TagModule],
